9901429076a412e83fe005b555583e046f70ea72
Jobet Casquejo <151836012+jobet9530@users.noreply.github.com>
2024-04-26 14:18:42 +0000

    Initial commit

---
+++ college/college/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-nj6!kh(57!(0eiy$6m=4wwy+#us__xv#__8(s8um6eq=qe7t+i"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
